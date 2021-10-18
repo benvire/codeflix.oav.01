a6de548ab99e5d3ea6a65c193639de215a22983f
Benjamin <benjamin.dupin@efrei.net>
2021-10-18 10:55:20 +0200

    modifying main.js

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+const { listenerCount } = require('events');
 const fs = require('fs')
-const env = require('../oav.01/php.env')
-
+const arg = process.argv[2]
 const variableReg = /[a-zA-Z_]+=.+/gm
-fs.readFile('../oav.01/php.ini', 'utf8' , (err, data) => {
+let file = `../oav.01/${arg}.JSON`
+
+
+fs.readFile(arg, 'utf8' , (err, data) => {
     if(err){
         console.log(err);
         return
     }
 
-    console.log(data);
-})
+
+    const converted = {}
+    while(null != (x = variableReg.exec(data))){
+        // console.log(x[0]);
+        xSplit = x[0].split('=')
+        
+        converted[xSplit[0]] = xSplit[1].replace(/\"/g, "")
 
 
+    };
 
+        fs.writeFileSync(`${arg}.JSON`, JSON.stringify(converted, null, 2));
+});
